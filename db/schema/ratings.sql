00b2a256b63b71869673db409ee0e6b3bf2232ad
Erica Sun <ericasun@seas.upenn.edu>
2020-07-31 23:04:31 +0000

    Complete schema

---
+++ db/schema/ratings.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS ratings CASCADE;
+
+CREATE TABLE (
+  id SERIAL PRIMARY KEY NOT NULL,
+  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  accessible_parking BOOLEAN DEFAULT FALSE,
+  accessible_washroom BOOLEAN DEFAULT FALSE,
+  alternative_entrance BOOLEAN DEFAULT FALSE,
+  automatic_door BOOLEAN DEFAULT FALSE,
+  elevator BOOLEAN DEFAULT FALSE,
+  braille BOOLEAN DEFAULT FALSE,
+  gender_neutal_washroom BOOLEAN DEFAULT FALSE,
+  large_print BOOLEAN DEFAULT FALSE,
+  outdoor_access_only BOOLEAN DEFAULT FALSE,
+  quiet BOOLEAN DEFAULT FALSE,
+  ramp BOOLEAN DEFAULT FALSE,
+  scent_free BOOLEAN DEFAULT FALSE,
+  service_animal_friendly BOOLEAN DEFAULT FALSE,
+  sign_language BOOLEAN DEFAULT FALSE,
+  spacious BOOLEAN DEFAULT FALSE,
+  stopgap_ramp BOOLEAN DEFAULT FALSE
+)
