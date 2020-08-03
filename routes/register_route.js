b18c1e13221a271afa3c87f9b3997a894ec2866a
Erica Sun <ericasun@seas.upenn.edu>
2020-08-03 00:13:28 +0000

    Fixed promise chaining in register

--- routes/register_route.js
+++ routes/register_route.js
@@ -20,39 +20,47 @@ module.exports = ({ addUser, isUserRegistered, getUserByEmail }) => {
   router.post("/", (req, res) => {
     console.log("hello from register");
     const { email, password, username } = req.body;
-    isUserRegistered(email).then(function (user) {
-      if (!user) {
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        addUser(email, hashedPassword, username).then(row => {
-          const userEmail = row.email 
-          getUserByEmail(userEmail).then((returnedUser) => {
-            console.log("email line 35:",userEmail)
-            const payload = {
-              user: {
-                id: returnedUser.id,
-              },
-            };
+    isUserRegistered(email)
+      .then((user) => {
+        if (!user) {
+          const hashedPassword = bcrypt.hashSync(password, 10);
+          addUser(email, hashedPassword, username)
+            .then((row) => {
+              const userEmail = row.email;
+              return getUserByEmail(userEmail);
+            })
+            .then((returnedUser) => {
+              const payload = {
+                user: {
+                  id: returnedUser.id,
+                },
+              };
 
-            jwt.sign(
-              payload,
-              process.env.JWTSECRET,
-              {
-                expiresIn: 360000,
-              }, //optional
-              (err, token) => {
-                if (err) throw err;
-                res.json({
-                  token,
-                });
-              }
-            );
-          });
-        });
-      } else {
-        res.status(400).send("Please login.");
-        return;
-      }
-    });
+              jwt.sign(
+                payload,
+                process.env.JWTSECRET,
+                {
+                  expiresIn: 360000,
+                }, //optional
+                (err, token) => {
+                  if (err) throw err;
+                  res.json({
+                    token,
+                  });
+                }
+              );
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          res.status(400).send("Please login.");
+          return;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
   return router;
