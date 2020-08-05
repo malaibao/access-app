2e4cfb64cc87a54d39307d9941c03e31d368f032
Erica Sun <ericasun@seas.upenn.edu>
2020-08-05 21:41:25 +0000

    change server port

--- client/src/components/basic-pages/Landing.js
+++ client/src/components/basic-pages/Landing.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import MapContainer from '../map-container/MapContainer';
+import React, { useState, useEffect } from "react";
+import MapContainer from "../map-container/MapContainer";
 
-import axios from 'axios';
+import axios from "axios";
 
 const Landing = () => {
   const [markpins, setPins] = useState([]);
   useEffect(() => {
     axios
-      .get('/pins')
-      .then((res) => setPins(res.data))
+      .get("/pins")
+      .then((res) => {
+        setPins(res.data);
+      })
       .catch((error) => console.log(error));
   }, []);
 
-  return (
-    <div>
-      <MapContainer pins={markpins} />
-    </div>
-  );
+  return <div>{<MapContainer pins={markpins} />}</div>;
 };
 
 export default Landing;
