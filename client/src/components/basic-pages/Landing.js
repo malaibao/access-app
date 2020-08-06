0d59415b0f733109a6fe06a3f8cde1eeac7705d4
bobby-brice <bobby.brice@gmail.com>
2020-08-06 16:53:07 +0000

    fix landing conflict

--- client/src/components/basic-pages/Landing.js
+++ client/src/components/basic-pages/Landing.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-=======
 import React, { useState, useEffect, useContext, useRef, useCallback } from "react";
->>>>>>> feature/panZoom
 import MapContainer from "../map-container/MapContainer";
 
 import axios from "axios";
