cb1ed9c2fa90ab4ff0c459f0742c1c3487c6e08d
Erica Sun <ericasun@seas.upenn.edu>
2020-08-06 12:09:46 -0400

    Resolve conflict again

--- client/src/components/basic-pages/Landing.js
+++ client/src/components/basic-pages/Landing.js
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import MapContainer from '../map-container/MapContainer';
-=======
 import React, { useState, useEffect } from "react";
 import MapContainer from "../map-container/MapContainer";
->>>>>>> user_route
 
-import axios from 'axios';
+import axios from "axios";
 
 const Landing = () => {
   const [markpins, setPins] = useState([]);
 
   useEffect(() => {
     axios
-      .get('/pins')
+      .get("/pins")
       .then((res) => {
         setPins(res.data);
       })
