0d4ebb617e6ea47da4729e00d21a48fa06c9b0e4
bobby-brice <bobby.brice@gmail.com>
2020-08-06 16:50:09 +0000

    add panzoom

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -6,7 +6,7 @@ import {
   Marker,
   InfoWindow,
 } from '@react-google-maps/api';
-import { formatRelative } from 'date-fns';
+// import { formatRelative } from 'date-fns';
 import mapStyles from './mapStyles';
 
 const libraries = ['places'];
@@ -27,7 +27,7 @@ const options = {
   zoomControl: true,
 };
 
-export default function Map({ pins }) {
+export default function Map({ pins, onMapLoad, chosen }) {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
@@ -36,11 +36,6 @@ export default function Map({ pins }) {
   const [markers, setMarkers] = React.useState([]);
   const [selected, setSelected] = React.useState(null);
 
-  const mapRef = React.useRef();
-
-  const onMapLoad = React.useCallback((map) => {
-    mapRef.current = map;
-  }, []);
 
   useEffect(() => {
     setMarkers(pins);
@@ -88,6 +83,13 @@ export default function Map({ pins }) {
                       {selected.address}
                     </div>
                   </InfoWindow>) : null}
+                  {chosen !== null ? <Marker key={'1234'} position={chosen} icon={{
+                    url:
+                      'https://e7.pngegg.com/pngimages/72/948/png-clipart-bulbasaur-pokemon-diamond-and-pearl-pokemon-go-pokedex-ivysaur-bulbasaur-pixel-vertebrate-grass.png',
+                    origin: new window.google.maps.Point(0, 0),
+                    anchor: new window.google.maps.Point(15, 15),
+                    scaledSize: new window.google.maps.Size(30, 30)
+                  }}/>: null}
         </GoogleMap>
       ) : (
         ''
