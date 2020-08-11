3e05507c616888761c6ea89715336adff6a8c112
Erica Sun <ericasun@seas.upenn.edu>
2020-08-11 12:35:22 -0400

    animate logo and change form colors

--- client/src/components/basic-pages/Form.js
+++ client/src/components/basic-pages/Form.js
@@ -1,21 +1,21 @@
-import React, { useState, useContext } from 'react';
-import { AuthContext, PinContext } from '../../context';
+import React, { useState, useContext } from "react";
+import { AuthContext, PinContext } from "../../context";
 
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import './Form.scss';
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import "./Form.scss";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
     width: 0.25,
   },
   formControl: {
@@ -27,8 +27,8 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1),
   },
   submit: {
-    backgroundColor: '#880f4f',
-    color: 'white',
+    backgroundColor: "#1660ff",
+    color: "white",
     marginLeft: 20,
   },
 }));
@@ -37,7 +37,7 @@ export default function Form({ pin, isOldPin }) {
   const [redirect, setRedirect] = useState(false);
   const { setPinInfo } = useContext(PinContext);
   const { authState, dispatch } = useContext(AuthContext);
-  const [submit, setSubmit] = useState(false); 
+  const [submit, setSubmit] = useState(false);
 
   const classes = useStyles();
   const [rating, setRating] = useState({
@@ -59,14 +59,14 @@ export default function Form({ pin, isOldPin }) {
     stopgap_ramp: false,
   });
 
-  const [errorInfo, setErrorInfo] = useState({ errMsg: '', show: false });
-  
+  const [errorInfo, setErrorInfo] = useState({ errMsg: "", show: false });
+
   if (redirect) {
-    return <Redirect to='/map' />;
+    return <Redirect to="/map" />;
   }
 
   if (submit) {
-    return <Redirect to = '/register' /> ;
+    return <Redirect to="/register" />;
   }
 
   const handleChange = (event) => {
@@ -77,7 +77,7 @@ export default function Form({ pin, isOldPin }) {
     setErrorInfo((prev) => ({ ...prev, show: false }));
   };
 
-  console.log('authstate', authState)
+  console.log("authstate", authState);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -88,7 +88,7 @@ export default function Form({ pin, isOldPin }) {
     if (filled === false) {
       setErrorInfo((prev) => ({
         ...prev,
-        errMsg: 'Select at least 1 option',
+        errMsg: "Select at least 1 option",
         show: true,
       }));
       return;
@@ -101,20 +101,19 @@ export default function Form({ pin, isOldPin }) {
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     };
 
     try {
-
       let res;
       if (isOldPin) {
-        res = await axios.post('/pins/ratings', pinInfo, config);
+        res = await axios.post("/pins/ratings", pinInfo, config);
       } else {
-        res = await axios.post('/pins', pinInfo, config);
+        res = await axios.post("/pins", pinInfo, config);
       }
 
-      setErrorInfo({ errMsg: '', show: false });
+      setErrorInfo({ errMsg: "", show: false });
       if (res.status === 200) {
         setPinInfo(null);
         setRedirect(true);
@@ -168,7 +167,7 @@ export default function Form({ pin, isOldPin }) {
     ].filter((v) => v).length < 1;
 
   return (
-    <div className='form'>
+    <div className="form">
       {pin ? (
         <>
           <h3>{pin.name}</h3>
@@ -176,96 +175,96 @@ export default function Form({ pin, isOldPin }) {
           <p>Type: {pin.type}</p>
         </>
       ) : null}
-      <form className={classes.form} id='rating-form' onSubmit={handleSubmit}>
-        <FormControl component='fieldset' className={classes.formControl}>
-          <FormLabel component='legend'>Select all that apply:</FormLabel>
+      <form className={classes.form} id="rating-form" onSubmit={handleSubmit}>
+        <FormControl component="fieldset" className={classes.formControl}>
+          <FormLabel component="legend">Select all that apply:</FormLabel>
           <FormGroup>
             <FormControlLabel
               control={
                 <Checkbox
                   checked={accessible_parking}
                   onChange={handleChange}
-                  name='accessible_parking'
+                  name="accessible_parking"
                 />
               }
-              label='accessible parking'
+              label="accessible parking"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={accessible_washroom}
                   onChange={handleChange}
-                  name='accessible_washroom'
+                  name="accessible_washroom"
                 />
               }
-              label='accessible washroom'
+              label="accessible washroom"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={alternative_entrance}
                   onChange={handleChange}
-                  name='alternative_entrance'
+                  name="alternative_entrance"
                 />
               }
-              label='alternative entrance'
+              label="alternative entrance"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={automatic_door}
                   onChange={handleChange}
-                  name='automatic_door'
+                  name="automatic_door"
                 />
               }
-              label='automatic door'
+              label="automatic door"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={elevator}
                   onChange={handleChange}
-                  name='elevator'
+                  name="elevator"
                 />
               }
-              label='elevator'
+              label="elevator"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={braille}
                   onChange={handleChange}
-                  name='braille'
+                  name="braille"
                 />
               }
-              label='braille'
+              label="braille"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={gender_neutral_washroom}
                   onChange={handleChange}
-                  name='gender_neutral_washroom'
+                  name="gender_neutral_washroom"
                 />
               }
-              label='gender neutral washroom'
+              label="gender neutral washroom"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={large_print}
                   onChange={handleChange}
-                  name='large_print'
+                  name="large_print"
                 />
               }
-              label='large print'
+              label="large print"
             />
           </FormGroup>
         </FormControl>
         <FormControl
           required
           error={error}
-          component='fieldset'
+          component="fieldset"
           className={classes.formControl}
           style={{ paddingTop: 18 }}
         >
@@ -275,81 +274,81 @@ export default function Form({ pin, isOldPin }) {
                 <Checkbox
                   checked={outdoor_access_only}
                   onChange={handleChange}
-                  name='outdoor_access_only'
+                  name="outdoor_access_only"
                 />
               }
-              label='outdoor access only'
+              label="outdoor access only"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={quiet}
                   onChange={handleChange}
-                  name='quiet'
+                  name="quiet"
                 />
               }
-              label='quiet'
+              label="quiet"
             />
             <FormControlLabel
               control={
-                <Checkbox checked={ramp} onChange={handleChange} name='ramp' />
+                <Checkbox checked={ramp} onChange={handleChange} name="ramp" />
               }
-              label='ramp'
+              label="ramp"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={scent_free}
                   onChange={handleChange}
-                  name='scent_free'
+                  name="scent_free"
                 />
               }
-              label='scent free'
+              label="scent free"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={service_animal_friendly}
                   onChange={handleChange}
-                  name='service_animal_friendly'
+                  name="service_animal_friendly"
                 />
               }
-              label='service animal friendly'
+              label="service animal friendly"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={sign_language}
                   onChange={handleChange}
-                  name='sign_language'
+                  name="sign_language"
                 />
               }
-              label='sign language'
+              label="sign language"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={spacious}
                   onChange={handleChange}
-                  name='spacious'
+                  name="spacious"
                 />
               }
-              label='spacious'
+              label="spacious"
             />
             <FormControlLabel
               control={
                 <Checkbox
                   checked={stopgap_ramp}
                   onChange={handleChange}
-                  name='stopgap_ramp'
+                  name="stopgap_ramp"
                 />
               }
-              label='stopgap ramp'
+              label="stopgap ramp"
             />
           </FormGroup>
         </FormControl>
         <br />
-        <Button type='submit' variant='contained' className={classes.submit}>
+        <Button type="submit" variant="contained" className={classes.submit}>
           Add Rating
         </Button>
       </form>
