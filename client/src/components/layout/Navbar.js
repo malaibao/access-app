b410d9be6a671c885d703568e302969a39c7785d
Maribelle Leong <maribelle.leong@gmail.com>
2020-08-11 22:43:16 -0300

    fixed link-to problem

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-import { AuthContext, PinContext } from "../../context";
-import { LOGOUT } from "../../reducers/action-types";
+import React, { useContext } from 'react';
+import { AuthContext, PinContext } from '../../context';
+import { LOGOUT } from '../../reducers/action-types';
 
-import { Link } from "react-router-dom";
-import PersonPinIcon from "@material-ui/icons/PersonPin";
+import { Link } from 'react-router-dom';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
 
-import "./Navbar.scss";
+import './Navbar.scss';
 
 const Navbar = () => {
   const { authState, dispatch } = useContext(AuthContext);
@@ -14,10 +14,14 @@ const Navbar = () => {
   const guestLink = (
     <ul>
       <li>
-        <Link to="/map">Map</Link>
+        <Link to='/map' onClick={() => setPinInfo(null)}>
+          Map
+        </Link>
       </li>
       <li>
-        <Link to="/login">Sign In</Link>
+        <Link to='/login' onClick={() => setPinInfo(null)}>
+          Sign In
+        </Link>
       </li>
     </ul>
   );
@@ -25,25 +29,29 @@ const Navbar = () => {
   const authLink = (
     <ul>
       <li>
-        <Link to="/map">Map</Link>
+        <Link to='/map' onClick={() => setPinInfo(null)}>
+          Map
+        </Link>
       </li>
       <li>
-        <Link to="/profile">Profile</Link>
+        <Link to='/profile' onClick={() => setPinInfo(null)}>
+          Profile
+        </Link>
       </li>
       <li>
-        <Link to="/" onClick={() => dispatch({ type: LOGOUT })}>
+        <Link to='/' onClick={() => dispatch({ type: LOGOUT })}>
           Sign Out
         </Link>
       </li>
     </ul>
   );
   return (
-    <nav className="navbar bg-dark">
-      <div className="logo">
+    <nav className='navbar bg-dark'>
+      <div className='logo'>
         <h1>
-          <Link to="/" onClick={() => setPinInfo(null)}>
+          <Link to='/' onClick={() => setPinInfo(null)}>
             Access for You
-            <PersonPinIcon fontSize="large" style={{ paddingTop: 10 }} />
+            <PersonPinIcon fontSize='large' style={{ paddingTop: 10 }} />
           </Link>
         </h1>
       </div>
--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,22 +1,22 @@
-import React, { useState, useContext } from "react";
-import { Redirect } from "react-router-dom";
-import { PinContext } from "../../context";
-import axios from "axios";
+import React, { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { PinContext } from '../../context';
+import axios from 'axios';
 
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
-} from "use-places-autocomplete";
+} from 'use-places-autocomplete';
 import {
   Combobox,
   ComboboxInput,
   ComboboxPopover,
   ComboboxList,
   ComboboxOption,
-} from "@reach/combobox";
+} from '@reach/combobox';
 
-import "@reach/combobox/styles.css";
-import "./Search.scss";
+import '@reach/combobox/styles.css';
+import './Search.scss';
 
 export default function Search({ panTo }) {
   const { pinInfo, setPinInfo } = useContext(PinContext);
@@ -47,44 +47,44 @@ export default function Search({ panTo }) {
       const results = await getGeocode({ address });
 
       const placeId = results[0].place_id; //we'll need this to check the db on search
-      console.log("from google", results[0].place_id);
+      console.log('from google', results[0].place_id);
 
       // check from our db with placeId
       const pinResult = await axios.get(`/pins/place_id/${placeId}`);
-      console.log("pinResult", pinResult);
+      console.log('pinResult', pinResult);
 
       const { lat, lng } = await getLatLng(results[0]);
       panTo({ lat, lng });
 
       setPinInfo(pinResult.data);
     } catch (error) {
-      console.log("An error has occurred", error);
+      console.log('An error has occurred', error);
     }
   };
 
   if (pinInfo) {
     if (pinInfo.found) {
-      return <Redirect to="/result" />;
+      return <Redirect to='/result' />;
     } else {
-      return <Redirect to="/new" />;
+      return <Redirect to='/new' />;
     }
   }
 
   return (
-    <div className="search">
-      <Combobox onSelect={handleSelect} className="combo-box">
+    <div className='search'>
+      <Combobox onSelect={handleSelect} className='combo-box'>
         <p>Search</p>
         <ComboboxInput
-          className="combo-box__input"
+          className='combo-box__input'
           value={value}
           onChange={handleInput}
           disabled={!ready}
-          placeholder={`Find by name or addresss`}
+          placeholder={`Find by name or address`}
           autoFocus={true}
         />
         <ComboboxPopover>
           <ComboboxList>
-            {status === "OK" &&
+            {status === 'OK' &&
               data.map(({ id, description }, index) => (
                 <ComboboxOption key={index} value={description} />
               ))}
